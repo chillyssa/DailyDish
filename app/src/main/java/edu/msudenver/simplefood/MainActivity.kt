da9af22e55ecab7f8cc8fa7af36225808cdef55f
karent2222 <61433860+karent2222@users.noreply.github.com>
2022-07-19 17:54:27 -0600

    added the CS3013 format

--- app/src/main/java/edu/msudenver/simplefood/MainActivity.kt
+++ app/src/main/java/edu/msudenver/simplefood/MainActivity.kt
@@ -1,4 +1,10 @@
 package edu.msudenver.simplefood
+/*
+* CS3013 - Mobile App Dev. - Summer 2022
+* Instructor: Thyago Mota
+* Student(s):Alyssa Williams, Brea Chaney & Karent Correa
+* Description:Final Project: DailyDish
+*/
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
