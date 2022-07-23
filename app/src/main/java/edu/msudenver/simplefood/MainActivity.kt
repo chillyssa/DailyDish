e74de1aa57e488112896232239ec857a4f62311f
brea_renee <brearenee@gmail.com>
2022-07-23 15:20:37 -0600

    main activity is now functional.

--- app/src/main/java/edu/msudenver/simplefood/MainActivity.kt
+++ app/src/main/java/edu/msudenver/simplefood/MainActivity.kt
@@ -1,11 +1,32 @@
 package edu.msudenver.simplefood
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+
+        val ingredientButton = findViewById<Button>(R.id.btnIngredients)
+        val recipeButton = findViewById<Button>(R.id.btnRecipes)
+
+        ingredientButton.setOnClickListener {
+            val intentIngredients = Intent(this, IngredientsActivity::class.java)
+            startActivity(intentIngredients)
+        }
+
+
+        recipeButton.setOnClickListener{
+            val intentRecipes = Intent(this, RecipeActivity::class.java)
+            startActivity(intentRecipes)
+        }
+
+}
 }
