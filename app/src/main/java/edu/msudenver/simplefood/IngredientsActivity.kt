ea7196369ac9b7164fb9c6b1821635906275c678
brea_renee <brearenee@gmail.com>
2022-07-23 14:54:26 -0600

    Created new activities for Recipes and Ingredients. Created their layout files, both with recycler views and their recycler view item layouts.
    layouts are super basic and can/should be adjusted as we move forward.

---
+++ app/src/main/java/edu/msudenver/simplefood/IngredientsActivity.kt
@@ -0,0 +1,11 @@
+package edu.msudenver.simplefood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IngredientsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingredients)
+    }
+}
